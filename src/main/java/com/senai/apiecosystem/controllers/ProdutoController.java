021bddc91c010cbf8fb519accc5e94a735a23a92
Cleber-Fsantos <cleber.cfsantos@gmail.com>
2023-11-05 15:12:11 -0300

    Update Metodos do ProdutoController

--- src/main/java/com/senai/apiecosystem/controllers/ProdutoController.java
+++ src/main/java/com/senai/apiecosystem/controllers/ProdutoController.java
@@ -1,7 +1,9 @@
 package com.senai.apiecosystem.controllers;
 
 
+import com.senai.apiecosystem.dtos.EnderecoDto;
 import com.senai.apiecosystem.dtos.ProdutoDto;
+import com.senai.apiecosystem.models.EnderecoModel;
 import com.senai.apiecosystem.models.ProdutoModel;
 import com.senai.apiecosystem.repositories.CategoriaRepository;
 import com.senai.apiecosystem.repositories.ProdutoRepository;
@@ -13,6 +15,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/produto", produces = {"application/json"})
@@ -26,6 +30,19 @@ public class ProdutoController {
     public ResponseEntity<List<ProdutoModel>> listarProdutos(){
         return ResponseEntity.status(HttpStatus.OK).body(produtoRepository.findAll());
     }
+
+    @GetMapping("/{idProduto}")
+    public ResponseEntity<Object> exibirProduto(@PathVariable(value = "idProduto") UUID id) {
+        Optional<ProdutoModel> produtoBuscado = produtoRepository.findById(id);
+
+        if (produtoBuscado.isEmpty()) {
+            // Retornar usuario não encontrado
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produtp não encontrado");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(produtoBuscado.get());
+    }
+
 
     @PostMapping
     public ResponseEntity<Object> cadastrarProduto(@RequestBody @Valid ProdutoDto produtoDto) {
@@ -42,6 +59,33 @@ public class ProdutoController {
         }
 
         return ResponseEntity.status(HttpStatus.CREATED).body(produtoRepository.save(produtoModel));
+    }
+
+    @PutMapping(value = "/{idProduto}")
+    public ResponseEntity<Object> editarProduto(@PathVariable(value = "idProduto") UUID id, @RequestBody @Valid ProdutoDto produtoDto) {
+        Optional<ProdutoModel> produtoBuscado = produtoRepository.findById(id);
+
+        if (produtoBuscado.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não encontrado");
+        }
+
+        ProdutoModel produto = produtoBuscado.get();
+        BeanUtils.copyProperties(produtoDto, produto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(produtoRepository.save(produto));
+    }
+
+    @DeleteMapping("/{idProduto}")
+    public ResponseEntity<Object> deletarProduto(@PathVariable(value = "idProduto") UUID id) {
+        Optional<ProdutoModel> produtoBuscado = produtoRepository.findById(id);
+
+        if (produtoBuscado.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não encontrado");
+        }
+
+        produtoRepository.delete(produtoBuscado.get());
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Produto deletado com sucesso!");
     }
 
 }
